2231c20f7532b6740a0abedb524a22a97a009d82
axlothecook <alliston.ruby123@gmail.com>
2025-07-15 17:02:55 +0200

    fixing add netlify stuff

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import ReactDOM from 'react-dom';
-import { PDFViewer } from '@react-pdf/renderer';
-// import Login from './failedLoginStuff/failedLogin.jsx'
-import Practice from './Practice.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <PDFViewer>
-      <MyDocument />
-    </PDFViewer> */}
     <App />
-    {/* <Login /> */}
-    {/* <Practice /> */}
   </StrictMode>,
 )
